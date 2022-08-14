f1e05139cf8dea7122d981842488d4a20ba8c391
Elizabeth Nguyen <saper209@yahoo.com>
2022-08-14 11:45:03 -0700

    Added Encounter

--- src/components/Encounter.js
+++ src/components/Encounter.js
@@ -1,19 +1,70 @@
-function Encounters({ trainer }){
+import Button from '@mui/material/Button';
+import { useEffect, useState } from "react"
+import { CardMedia } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-  function randomPokemon(id){
+function Encounters({ trainer }){
+  const [randomPokemon, setRandomPokemon] = useState([])
+
+  useEffect(() => {
+    let id = getRandomNumber()
+    getNewPokemon(id)
+  }, [])
+
+  function getRandomNumber(){
+    return Math.floor(Math.random() * (500 - 1) + 1)
+  }
+
+  function onRandomPokemon(newPokemon){
+    setRandomPokemon(newPokemon)
+  }
+
+  function getNewPokemon(id){
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then(res => res.json())
-    .then(data => {
-      let name = data.name
-      let url = data.sprites.other.dream_world.front_default
-      if (name && url) {
-        
-      }
-    })
+    .then(data => onRandomPokemon({
+      name: data.name,
+      url: data.sprites.other.dream_world.front_default
+    }))
+  }
+
+  function catchPokemon(){
+    fetch("http://localhost:9292/pokemons", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: randomPokemon.name,
+        species: randomPokemon.name,
+        trainer_id: trainer
+      })
+    }).then((res) => res.json())
+    .then((data) => console.log(data))
   }
 
   return (
     <>
+    <Card
+      sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}
+    >
+      <CardMedia 
+        component="img"
+        sx={{
+          pt: '56.25%'
+        }}
+        image={randomPokemon.url}
+      />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {randomPokemon.name}
+        </Typography>
+      </CardContent>
+    </Card>
+    <Button onClick={() => getNewPokemon(getRandomNumber())}>New Pokemon</Button>
+    <Button onClick={catchPokemon}>Catch</Button>
     </>
   )
 }
--- src/components/Roster.js
+++ src/components/Roster.js
@@ -8,33 +8,48 @@ import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
 
-function Roster({ trainer }){
+function Roster({ trainer }) {
   const [pokemons, setPokemons] = useState([])
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetch(`http://localhost:9292/trainers/${trainer}`)
-    .then((res) => res.json())
-    .then((res) => onPokemons(res.pokemons))
+      .then((res) => res.json())
+      .then((res) => onPokemons(res.pokemons))
   }, [])
 
-  function onPokemons(newPokemons){
+  function onPokemons(newPokemons) {
     setPokemons(newPokemons)
   }
 
-  function releasePokemon(id){
+  function releasePokemon(id) {
     fetch(`http://localhost:9292/pokemons/${id}`, {
       method: "DELETE"
     }).then((res) => res.json())
-    .then((res) => onReleasePokemon(res))
+      .then((res) => onReleasePokemon(res))
   }
 
-  function onReleasePokemon(pokemon){
+  function onReleasePokemon(pokemon) {
     let newPokemons = pokemons.filter((currentPokemon) => currentPokemon.id !== pokemon.id)
     setPokemons(newPokemons)
   }
 
+  function renamePokemon(pokemon){
+    
+  }
+
   return (
     <Container sx={{ py: 8 }} maxWidth="md">
+      <div style={{ padding: 20 }}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button onClick={() => navigate("/encounter")} variant="contained">Find More Pokemon</Button>
+        </Grid>
+      </div>
       <Grid container spacing={4} >
         {pokemons.map((pokemon) => (
           <Grid item xs={12} sm={6} md={4}>
@@ -51,6 +66,7 @@ function Roster({ trainer }){
               </CardContent>
               <CardActions>
                 <Button onClick={() => releasePokemon(pokemon.id)} size="small">Release</Button>
+                <Button onClick={() => renamePokemon(pokemon.id)} size="small">Rename</Button>
               </CardActions>
             </Card>
           </Grid>
